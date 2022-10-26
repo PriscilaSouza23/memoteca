a63722ad0ec1ee752babb3bac6435bb35a4c0bad
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-10-25 22:47:35 -0300

    project finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
-import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { ListaPensamentosComponent } from './components/pensamentos/listar-pensamento/lista-pensamentos.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'listarPensamento',
+    redirectTo: 'listaPensamentos',
     pathMatch: 'full'
   },
   {
@@ -14,9 +16,17 @@ const routes: Routes = [
     component: CriarPensamentoComponent,
   },
   {
-    path: 'listarPensamento',
-    component: ListarPensamentoComponent,
+    path: 'listaPensamentos',
+    component: ListaPensamentosComponent,
   },
+  {
+    path: 'pensamentos/excluirPensamentos/:id',
+    component: ExcluirPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/editarPensamentos/:id',
+    component: EditarPensamentoComponent,
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,11 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { FormsModule } from '@angular/forms';
-import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
+import { ListaPensamentosComponent } from './components/pensamentos/listar-pensamento/lista-pensamentos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,16 @@ import { PensamentoComponent } from './components/pensamentos/pensamento/pensame
     CabecalhoComponent,
     RodapeComponent,
     CriarPensamentoComponent,
-    ListarPensamentoComponent,
-    PensamentoComponent
+    ListaPensamentosComponent,
+    PensamentoComponent,
+    ExcluirPensamentoComponent,
+    EditarPensamentoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
